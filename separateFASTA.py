44e402680255216f34795911a60c5ecf41530551
Szilveszter Juhos <szilveszter.juhos@scilifelab.se>
2017-01-17 09:26:36 +0100

    removing HLA: from ref ids

--- separateFASTA.py
+++ separateFASTA.py
@@ -8,6 +8,9 @@ def separateFasta(fasta):
     """Utility to separate a multi-FASTA to separate FASTA files"""
     
     for seq_record in SeqIO.parse(fasta, "fasta"):
+        # we are changing the IDs from "HLA:HLA..." to "HLA...". This makes IGV and other tools much happier
+        # would be nice to know what lead to this idiocity in naming conventions
+        seq_record.id = seq_record.id.replace("HLA:","")
         print("writing " + seq_record.id)
         SeqIO.write(seq_record,seq_record.id + ".fasta", "fasta")
 
